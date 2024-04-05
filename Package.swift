7575f0009a7163614f542a9f4a1f96a99548e489
Livsy90 <artmir@live.ru>
2024-04-05 13:17:51 +0400

    WPDataSource

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "WPDataSource",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WPDataSource",
             targets: ["WPDataSource"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Livsy90/SwiftPresso.git", .upToNextMajor(from: "1.0.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WPDataSource"),
+            name: "WPDataSource",
+            dependencies: [
+                "SwiftPresso"
+            ]
+        ),
         .testTarget(
             name: "WPDataSourceTests",
             dependencies: ["WPDataSource"]),
--- Sources/WPDataSource/WPDataSource.swift
+++ Sources/WPDataSource/WPDataSource.swift
@@ -1,2 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftPresso
+
+public struct WPDataSource {
+    
+    private let manager: PostListManagerProtocol
+    
+    public init(
+        url: URL,
+        httpScheme: HTTPScheme,
+        additionalHeader: [AnyHashable: Any]?
+    ) {
+        
+        manager = SwiftPressoFactory.makePostListManager(
+            url: url,
+            httpScheme: httpScheme,
+            httpAdditionalHeaders: additionalHeader
+        )
+    }
+    
+    public func getPostList(page: Int) async -> Result<[RefinedPost], Error> {
+        do {
+            let postList = try await manager.getPosts(pageNumber: page)
+            return .success(postList)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+}
